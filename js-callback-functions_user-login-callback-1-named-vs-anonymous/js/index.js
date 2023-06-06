3f4bb570f5e5b1308e073e746e4b24d529553d87
swethakeerthipati <swethakeerthipati@gmail.com>
2023-06-06 10:17:15 +0200

    Add logged in now challenge

---
+++ js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -0,0 +1,15 @@
+console.clear();
+
+function handleUserLogin(onSuccess) {
+  // You might wonder why this function is called `onSuccess`. This will become clear by the fourth challenge! 🤞
+  onSuccess();
+}
+
+// Call handleUserLogin below!
+
+function showWelcomeMessage() {
+  handleUserLogin(() => {
+    console.log("Welcome! You are logged in now.");
+  });
+}
+handleUserLogin(showWelcomeMessage);
