4a18d416f3bb7e22d939a6e4cabff34834f65fdb
swethakeerthipati <swethakeerthipati@gmail.com>
2023-06-06 11:05:11 +0200

    Add callback-functions challenge

---
+++ js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
@@ -0,0 +1,13 @@
+console.clear();
+
+function handleUserLogin(onSuccess) {
+  onSuccess("Jane Doe", "admin");
+}
+
+// The exercise starts here!
+
+function showWelcomeMessage(userName, userRole) {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+}
+
+handleUserLogin(showWelcomeMessage);
